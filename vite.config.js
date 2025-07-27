30829ae1c03bcc73262498526e21258ad39bb406
Alessandro <jamancapiero85@gmail.com>
2025-07-26 22:00:13 -0300

    Ok

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Asegúrate de que coincida con el directorio que sirve
+  },
+  server: {
+    port: process.env.PORT || 3000,
+    host: true, // Permite acceso externo
+  },
+});
